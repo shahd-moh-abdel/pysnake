232f5943e8dd4184c2c84a96e396c4c3110969f3
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-04-16 21:02:04 +0300

    It is moving!

--- first.py
+++ first.py
@@ -4,8 +4,13 @@ pygame.init()
 
 # Constants
 WIDTH, HEIGHT = 500, 500
+x = 100
+y = 100
+speed = 50
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My First Game")
+
+
 
 while True:
     for event in pygame.event.get():
@@ -18,6 +23,16 @@ while True:
                 exit()
             elif event.key == pygame.K_SPACE:
                 print("Space key pressed!")
-    pygame.draw.rect(screen, (255, 0, 0), (100, 100, 50, 50))
+
+            elif event.key == pygame.K_UP:
+                y -= speed
+            elif event.key == pygame.K_DOWN:
+                y += speed 
+            elif event.key == pygame.K_LEFT:
+                x -= speed
+            elif event.key == pygame.K_RIGHT:
+                x += speed
+    screen.fill((0, 0, 0)) 
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, 50, 50))
     pygame.display.flip()
     
